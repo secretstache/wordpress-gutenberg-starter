043c01a6a3f1d599297dc22eff68a3eed4956c33
Bukita89 <maria@secretstache.com>
2024-07-15 21:52:39 +0300

    Theme Boilerplate: add files

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-	
-	{!! the_content() !!}
 
+	{!! the_content() !!}
+	
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,13 @@
-<a class="sr-only focus:not-sr-only" href="#main">
-  {{ __('Skip to content') }}
-</a>
+@include('partials.offcanvas')
+	
+@include('partials.header')
 
-@include('sections.header')
+<div class="container">
 
-  <main id="main" class="main">
-    @yield('content')
-  </main>
+	<main class="content" id="main">
+		@yield('content')
+	</main>
 
-  @hasSection('sidebar')
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
-  @endif
+</div>
 
-@include('sections.footer')
+@include('partials.footer')
