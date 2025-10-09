25bae2fde8fea8d37e6a69e4b10c75f432ea6ddb
Dima Fedorenko <dev.fedorenko@gmail.com>
2025-10-09 19:44:26 +0300

    Add "Skip to Main Content" link

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="utf-8">
-    <?php wp_head(); ?>
-  </head>
+    <head>
+        <meta charset="utf-8">
+        <?php wp_head(); ?>
+    </head>
 
-  <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <?php do_action('get_header'); ?>
+    <body <?php body_class(); ?>>
+        <a href="#main" class="absolute left-2 -top-full z-[99999] bg-black text-white font-bold px-4 py-2 rounded border-2 border-transparent no-underline transition-[top] duration-300 ease-in-out focus:top-2 focus:outline-none focus:border-white hover:focus:bg-[#1f2937]">Skip to Main Content</a>
 
-    <div id="app">
-      <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
-    </div>
+        <?php wp_body_open(); ?>
 
-    <?php do_action('get_footer'); ?>
-    <?php wp_footer(); ?>
-  </body>
+        <div class="site flex flex-col min-h-screen *:w-full">
+
+            <?php do_action('get_header'); ?>
+
+            <?php echo view(app('sage.view'), app('sage.data'))->render(); ?>
+
+            <?php do_action('get_footer'); ?>
+
+        </div>
+
+        <?php wp_footer(); ?>
+    </body>
 </html>
