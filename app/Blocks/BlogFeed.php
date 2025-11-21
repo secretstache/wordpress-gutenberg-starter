13fd85a301a2d34589145e5b41b27f6c7a664f0c
Dima Fedorenko <dev.fedorenko@gmail.com>
2025-11-21 17:04:18 +0200

    - Blog Feed: update controls - Update `@secretstache/wordpress-gutenberg` dependency

--- app/Blocks/BlogFeed.php
+++ app/Blocks/BlogFeed.php
@@ -5,28 +5,43 @@ use App\View\Composers\SSM;
 
 class BlogFeed extends Block
 {
+    public const QUERY_LATEST = 'latest';
+    public const QUERY_CURATED = 'curated';
+    public const QUERY_BY_CATEGORY = 'by_category';
 
     protected function prepareData($attributes, $content): array
     {
-        $query              = $attributes['queryType'] ?? 'latest';
+        $query              = $attributes['queryType'] ?? self::QUERY_LATEST;
         $number_posts       = $attributes['numberOfPosts'] ?? -1;
         $curated_posts      = $attributes['curatedPosts'] ?? [];
-        $curated_terms      = $attributes['curatedTermsIds'] ?? [];
+        $curated_terms      = $attributes['curatedTerms'] ?? [];
+        $is_show_filters    = $attributes['isShowFilters'] ?? false;
 
-        $posts = SSM::getPosts( [
+        $args = [
             'data_source'       => 'posts',
             'query'             => $query,
             'taxonomy_slug'     => 'category',
             'curated_terms'     => $curated_terms,
-            'number_posts'      => $number_posts,
+            'number_posts'      => $query === self::QUERY_CURATED ? -1 : $number_posts,
             'curated_posts'     => $curated_posts,
+            'excluded_posts'    => [get_the_ID()],
             'prefix'            => 'ssm'
-        ]);
+        ];
+
+        $is_empty_selection = ($query === self::QUERY_CURATED && empty($curated_posts)) ||
+            ($query === self::QUERY_BY_CATEGORY && empty($curated_terms)) ||
+            ($query !== self::QUERY_CURATED && $number_posts === 0);
+
+        if ($is_show_filters && $query === self::QUERY_LATEST) {
+            $extra_args = ['facetwp' => true];
+        }
+
+        $posts = $is_empty_selection ? [] : SSM::getPosts($args, $extra_args ?? []);
 
         $data = [
-            'wrapper_attributes'    => get_block_wrapper_attributes(['class' => 'wp-block-ssm-blog-feed']),
+            'wrapper_attributes'    => get_block_wrapper_attributes(),
             'posts'                 => $posts,
-            'number_posts'          => $number_posts,
+            'include_filters'       => $is_show_filters && $query === self::QUERY_LATEST,
         ];
 
         return $data;
--- resources/scripts/editor/blocks/blog-feed/edit.jsx
+++ resources/scripts/editor/blocks/blog-feed/edit.jsx
@@ -1,234 +1,91 @@
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
-import {
-    PanelBody,
-    BaseControl,
-    __experimentalDivider as Divider,
-    __experimentalVStack as VStack,
-    RangeControl,
-    RadioControl,
-    CheckboxControl,
-} from '@wordpress/components';
-import { useCallback, useEffect, useState } from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
-import { arrayMove } from 'react-sortable-hoc';
-import Select from 'react-select';
+import { select } from '@wordpress/data';
+import { useMemo } from '@wordpress/element';
+import { PanelBody, ToggleControl } from '@wordpress/components';
+import { ResourcesWrapper, DataQueryControls, useDataQuery } from '@secretstache/wordpress-gutenberg';
 
-import {
-    SortableSelectAsync,
-    loadSelectOptions,
-    useDataQuery,
-    ResourcesWrapper,
-    decodeHtmlEntities,
-} from '@secretstache/wordpress-gutenberg';
+const QUERY_TYPE = {
+    LATEST: 'latest',
+    BY_CATEGORY: 'by_category',
+    CURATED: 'curated',
+};
 
-import { LAYOUT_TYPE, POST_TYPE, QUERY_TYPE } from './index.jsx';
-import { PostsList } from './components/PostsList.jsx';
+const POST_TYPE = { POST: 'post' };
+
+const TAXONOMY = { CATEGORY: 'categories' };
 
 export const edit = ({ attributes, setAttributes }) => {
+    const blockProps = useBlockProps();
+
     const {
         queryType,
-        selectedCategories,
+        curatedTerms,
         curatedPosts,
         numberOfPosts,
-        layoutType,
-        isShowFeaturedImage,
-        isShowTitle,
-        isShowPostMeta,
-        columnsPerRow,
+        isShowFilters
     } = attributes;
 
-    const resourceCategories = useSelect((select) => {
-        return select('core').getEntityRecords('taxonomy', 'category', { per_page: -1 });
-    }, []);
-
-    const resourceCategoriesOptions = resourceCategories?.map(category => ({
-        value: category.id,
-        label: decodeHtmlEntities(category.name),
-    })) || [];
-
-    const loadResourcesOptions = useCallback(async (inputValue) => {
-        return await loadSelectOptions(inputValue, POST_TYPE.RESOURCES);
-    }, []);
-
-    const [ defaultResourcesOptions, setDefaultResourcesOptions ] = useState([]);
-
-    useEffect(() => {
-        loadSelectOptions('', POST_TYPE.RESOURCES).then(setDefaultResourcesOptions);
-    }, []);
-
-    const onSortEnd = ({ oldIndex, newIndex }) => {
-        const newCuratedPosts = arrayMove(curatedPosts, oldIndex, newIndex);
-        setAttributes({ curatedPosts: newCuratedPosts });
-    };
-
-    const onQueryTypeChange = useCallback((queryType) => setAttributes({
-        queryType,
-        selectedCategories: [],
-        curatedPosts: [],
-    }), []);
-
-    const onCuratedPostsChange = useCallback((curatedPosts) => setAttributes({ curatedPosts }), []);
-
-    const onLayoutTypeChange = useCallback((layoutType) => setAttributes({ layoutType }), []);
-
-    const onResourcesCategoryChange = useCallback((selectedOptions) => {
-        const selectedIds = selectedOptions.map(option => option.value);
-        setAttributes({ selectedCategories: selectedIds });
-    }, []);
-
-    const onNumberOfPostsChange = useCallback((numberOfPosts) => setAttributes({ numberOfPosts }), []);
-
-    const onColumnsPerRowChange = useCallback((columnsPerRow) => setAttributes({ columnsPerRow }), []);
-
-    const onIsShowFeaturedImageChange = useCallback((isShowFeaturedImage) => setAttributes({ isShowFeaturedImage }), []);
-
-    const onIsShowTitleChange = useCallback((isShowTitle) => setAttributes({ isShowTitle }), []);
-
-    const onIsShowPostMetaChange = useCallback((isShowPostMeta) => setAttributes({ isShowPostMeta }), []);
-
     const isQueryTypeAll = queryType === QUERY_TYPE.LATEST;
-    const isQueryTypeByCategory = queryType === QUERY_TYPE.BY_CATEGORY;
     const isQueryTypeCurated = queryType === QUERY_TYPE.CURATED;
+    const isQueryTypeByCategory = queryType === QUERY_TYPE.BY_CATEGORY;
 
-    const isEmptySelection =
-        (isQueryTypeCurated && !curatedPosts?.length) ||
-        (isQueryTypeByCategory && !selectedCategories?.length);
+    const isEmptySelection = (isQueryTypeCurated && !curatedPosts?.length) || (isQueryTypeByCategory && !curatedTerms?.length);
 
-    const {
-        postsToShow,
-        isResolving,
-        isEmpty,
-    } = useDataQuery({
-        postType: 'post',
-        curatedPostsIds: isQueryTypeCurated && curatedPosts?.map((post) => post?.value),
-        taxonomySlug: 'categories',
-        curatedTermsIds: selectedCategories,
-        numberOfPosts: isQueryTypeAll ? numberOfPosts : -1,
-    }, [ queryType, selectedCategories, curatedPosts, numberOfPosts ]);
+    const currentPostID = select('core/editor').getCurrentPostId();
 
-    const isBlockGrid = layoutType === LAYOUT_TYPE.BLOCK_GRID;
+    const queryConfig = useMemo(() => ({
+        postType: POST_TYPE.POST,
+        taxonomySlug: TAXONOMY.CATEGORY,
+        curatedTermsIds: isQueryTypeByCategory && curatedTerms,
+        curatedPostsIds: isQueryTypeCurated && curatedPosts?.map((post) => post.value),
+        numberOfPosts: !isQueryTypeCurated ? numberOfPosts : -1,
+        extraQueryArgs: (isQueryTypeAll || isQueryTypeByCategory) ? { order: 'asc', orderby: 'title', exclude: currentPostID } : {},
+    }), [ queryType, curatedTerms, curatedPosts, numberOfPosts ]);
 
-    const blockProps = useBlockProps();
+    const { postsToShow, isResolving, isEmpty } = useDataQuery(queryConfig, [ queryType, curatedTerms, curatedPosts, numberOfPosts ]);
+
+    const onIsShowFiltersChange = () => setAttributes({ isShowFilters: !isShowFilters })
 
     return (
         <>
             <InspectorControls>
                 <PanelBody title="Settings">
-
-                    <RadioControl
-                        label="Layout"
-                        selected={layoutType}
-                        options={[
-                            { label: 'Block Grid', value: LAYOUT_TYPE.BLOCK_GRID },
-                            { label: 'Stacked', value: LAYOUT_TYPE.STACKED },
-                        ]}
-                        onChange={onLayoutTypeChange}
-                    />
-
-                    {
-                        isBlockGrid && (
-                            <RangeControl
-                                label="Columns Per Row"
-                                value={columnsPerRow}
-                                onChange={onColumnsPerRowChange}
-                                min={2}
-                                max={4}
-                            />
-                        )
-                    }
-
-                    <BaseControl
-                        label="UI Elements"
-                        __nextHasNoMarginBottom={true}
+                    <DataQueryControls
+                        attributes={attributes}
+                        setAttributes={setAttributes}
                     >
-                        <VStack>
-                            <CheckboxControl
-                                label="Show Featured Image"
-                                checked={isShowFeaturedImage}
-                                onChange={onIsShowFeaturedImageChange}
-                                __nextHasNoMarginBottom={true}
-                            />
+                        <DataQueryControls.QueryType
+                            attributeName="queryType"
+                            options={[
+                                { label: 'Latest', value: QUERY_TYPE.LATEST },
+                                { label: 'Latest by Category', value: QUERY_TYPE.BY_CATEGORY },
+                                { label: 'Curated', value: QUERY_TYPE.CURATED },
+                            ]} />
 
-                            <CheckboxControl
-                                label="Show Title"
-                                checked={isShowTitle}
-                                onChange={onIsShowTitleChange}
-                                __nextHasNoMarginBottom={true}
-                            />
+                        <DataQueryControls.TaxonomySelect
+                            condition={isQueryTypeByCategory}
+                            attributeName="curatedTerms"
+                            placeholder="Categories to show"
+                            postType={TAXONOMY.CATEGORY}
+                        />
 
-                            <CheckboxControl
-                                label="Show Post Meta"
-                                checked={isShowPostMeta}
-                                onChange={onIsShowPostMetaChange}
-                                __nextHasNoMarginBottom={true}
-                            />
-                        </VStack>
-                    </BaseControl>
+                        <DataQueryControls.CuratedPosts
+                            condition={isQueryTypeCurated}
+                            attributeName="curatedPosts"
+                            postType={POST_TYPE.POST}
+                        />
 
-                    <RadioControl
-                        label="Query"
-                        selected={queryType}
-                        options={[
-                            { label: 'Latest', value: QUERY_TYPE.LATEST },
-                            { label: 'Latest by Category', value: QUERY_TYPE.BY_CATEGORY },
-                            { label: 'Curated', value: QUERY_TYPE.CURATED },
-                        ]}
-                        onChange={onQueryTypeChange}
+                        <DataQueryControls.NumberOfPosts
+                            condition={!isQueryTypeCurated}
+                            attributeName="numberOfPosts"
+                        />
+                    </DataQueryControls>
+
+                    <ToggleControl
+                        label="Include Filters"
+                        checked={isShowFilters}
+                        onChange={onIsShowFiltersChange}
                     />
-
-                    {
-                        ( isQueryTypeCurated ) && (
-                            <>
-                                <Divider margin={2} />
-
-                                <SortableSelectAsync
-                                    onSortEnd={onSortEnd}
-                                    value={curatedPosts}
-                                    defaultOptions={defaultResourcesOptions}
-                                    loadOptions={loadResourcesOptions}
-                                    onChange={onCuratedPostsChange}
-                                    placeholder="Resources to show"
-                                />
-                            </>
-                        )
-                    }
-
-                    {
-                        ( isQueryTypeByCategory ) && (
-                            <>
-                                <Divider margin={2} />
-
-                                <Select
-                                    isMulti
-                                    options={resourceCategoriesOptions}
-                                    value={selectedCategories?.map(id => resourceCategoriesOptions.find(option => option.value === id))}
-                                    onChange={onResourcesCategoryChange}
-                                    className="react-select-container"
-                                    classNamePrefix="react-select"
-                                    placeholder="Categories to show"
-                                />
-
-                            </>
-                        )
-                    }
-
-                    {
-                        ( isQueryTypeAll || isQueryTypeByCategory ) && (
-                            <>
-                                <Divider margin={2}/>
-
-                                <RangeControl
-                                    label="Number of Posts"
-                                    value={numberOfPosts}
-                                    onChange={onNumberOfPostsChange}
-                                    min={-1}
-                                    max={12}
-                                    help="The maximum number of posts to show (-1 for no limit)"
-                                />
-                            </>
-                        )
-                    }
-
                 </PanelBody>
             </InspectorControls>
 
@@ -238,17 +95,15 @@ export const edit = ({ attributes, setAttributes }) => {
                     isEmptyResources={isEmpty}
                     isEmptySelection={isEmptySelection}
                 >
-                    <PostsList
-                        posts={postsToShow}
-                        layoutType={layoutType}
-                        isShowFeaturedImage={isShowFeaturedImage}
-                        isShowTitle={isShowTitle}
-                        isShowPostMeta={isShowPostMeta}
-                        columnsPerRow={columnsPerRow}
-                        isBlockGrid={isBlockGrid}
-                    />
+                    <ul>
+                        {
+                            postsToShow?.map((post) => (
+                                <li key={post.id}>{post.title?.rendered}</li>
+                            ))
+                        }
+                    </ul>
                 </ResourcesWrapper>
             </div>
         </>
     );
-};
+}
