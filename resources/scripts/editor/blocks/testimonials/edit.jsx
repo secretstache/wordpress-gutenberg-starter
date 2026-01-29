b8a0631b25893b3ad212ef499d951fc30b1b3e58
Dima Fedorenko <dev.fedorenko@gmail.com>
2026-01-29 19:35:47 +0200

    Testimonials: refactoring

--- resources/scripts/editor/blocks/testimonials/edit.jsx
+++ resources/scripts/editor/blocks/testimonials/edit.jsx
@@ -17,33 +17,33 @@ const QUERY_TYPE = {
 const POST_TYPE = { TESTIMONIAL: 'ssm_testimonial' };
 
 export const edit = ({ attributes, setAttributes }) => {
-	const {
-		queryType,
-		curatedPosts,
-		numberOfPosts,
-	} = attributes;
+    const {
+        queryType,
+        curatedPosts,
+        numberOfPosts,
+    } = attributes;
 
-	const isQueryTypeAll = queryType === QUERY_TYPE.ALL;
-	const isQueryTypeCurated = queryType === QUERY_TYPE.CURATED;
+    const isQueryTypeAll = queryType === QUERY_TYPE.ALL;
+    const isQueryTypeCurated = queryType === QUERY_TYPE.CURATED;
 
-	const isEmptySelection = isQueryTypeCurated && !curatedPosts?.length;
+    const isEmptySelection = isQueryTypeCurated && !curatedPosts?.length;
 
-	const queryConfig = useMemo(() => ({
-		postType: POST_TYPE.TESTIMONIAL,
-		curatedPostsIds: isQueryTypeCurated ? curatedPosts?.map((post) => post.value) : undefined,
-		numberOfPosts: isQueryTypeCurated ? -1 : numberOfPosts,
-		extraQueryArgs: isQueryTypeAll ? { order: 'asc', orderby: 'title' } : {},
-	}), [ queryType, curatedPosts, numberOfPosts ]);
+    const queryConfig = useMemo(() => ({
+        postType: POST_TYPE.TESTIMONIAL,
+        curatedPostsIds: isQueryTypeCurated ? curatedPosts?.map((post) => post.value) : undefined,
+        numberOfPosts: isQueryTypeCurated ? -1 : numberOfPosts,
+        extraQueryArgs: isQueryTypeAll ? { order: 'asc', orderby: 'title' } : {},
+    }), [ queryType, curatedPosts, numberOfPosts ]);
 
-	const { postsToShow, isResolving, isEmpty } = useDataQuery(
-		queryConfig,
-		[ queryType, curatedPosts, numberOfPosts ],
-	);
+    const { postsToShow, isResolving, isEmpty } = useDataQuery(
+        queryConfig,
+        [ queryType, curatedPosts, numberOfPosts ],
+    );
 
-	const blockRef = useRef(null);
-	const sliderInstance = useRef(null);
+    const blockRef = useRef(null);
+    const sliderInstance = useRef(null);
 
-	useEffect(() => {
+    useEffect(() => {
         if (postsToShow?.length > 0 && !isResolving && !isEmpty && blockRef?.current && !sliderInstance.current) {
             sliderInstance.current = initTestimonials(blockRef.current);
         }
@@ -56,57 +56,57 @@ export const edit = ({ attributes, setAttributes }) => {
         };
     }, [ postsToShow, isResolving, isEmpty, blockRef ]);
 
-	const blockProps = useBlockProps({
-		ref: blockRef,
-	});
+    const blockProps = useBlockProps({
+        ref: blockRef,
+    });
 
-	return (
-		<>
-			<InspectorControls>
-				<PanelBody title="Settings">
-					<DataQueryControls
-						attributes={attributes}
-						setAttributes={setAttributes}
-					>
-						<DataQueryControls.QueryType
-							attributeName="queryType"
-							options={[
-								{ label: 'All', value: QUERY_TYPE.ALL },
-								{ label: 'Curated', value: QUERY_TYPE.CURATED },
-							]}
-						/>
+    return (
+        <>
+            <InspectorControls>
+                <PanelBody title="Settings">
+                    <DataQueryControls
+                        attributes={attributes}
+                        setAttributes={setAttributes}
+                    >
+                        <DataQueryControls.QueryType
+                            attributeName="queryType"
+                            options={[
+                                { label: 'All', value: QUERY_TYPE.ALL },
+                                { label: 'Curated', value: QUERY_TYPE.CURATED },
+                            ]}
+                        />
 
-						<DataQueryControls.CuratedPosts
-							condition={isQueryTypeCurated}
-							attributeName="curatedPosts"
-							postType={POST_TYPE.TESTIMONIAL}
-							placeholder="Testimonials to show"
-						/>
+                        <DataQueryControls.CuratedPosts
+                            condition={isQueryTypeCurated}
+                            attributeName="curatedPosts"
+                            postType={POST_TYPE.TESTIMONIAL}
+                            placeholder="Testimonials to show"
+                        />
 
-						<DataQueryControls.NumberOfPosts
-							condition={!isQueryTypeCurated}
-							attributeName="numberOfPosts"
-							hasDivider={false}
-						/>
-					</DataQueryControls>
-				</PanelBody>
-			</InspectorControls>
+                        <DataQueryControls.NumberOfPosts
+                            condition={!isQueryTypeCurated}
+                            attributeName="numberOfPosts"
+                            hasDivider={false}
+                        />
+                    </DataQueryControls>
+                </PanelBody>
+            </InspectorControls>
 
-			<div {...blockProps}>
-				<ResourcesWrapper
-					isLoading={isResolving}
-					isEmptyResources={isEmpty}
-					isEmptySelection={isEmptySelection}
-				>
-					<div className="splide" role="group" aria-label="Tesimonials">
-            			<div className="splide__track">
-                			<div className="splide__list">
-								{postsToShow?.map((testimonial) => <Testimonial key={testimonial.id} testimonial={testimonial} />)}
-							</div>
-						</div>
-					</div>
-				</ResourcesWrapper>
-			</div>
-		</>
-	);
-}
+            <div {...blockProps}>
+                <ResourcesWrapper
+                    isLoading={isResolving}
+                    isEmptyResources={isEmpty}
+                    isEmptySelection={isEmptySelection}
+                >
+                    <div className="splide" role="group" aria-label="Tesimonials">
+                        <div className="splide__track">
+                            <div className="splide__list">
+                                {postsToShow?.map((testimonial) => <Testimonial key={testimonial.id} testimonial={testimonial} />)}
+                            </div>
+                        </div>
+                    </div>
+                </ResourcesWrapper>
+            </div>
+        </>
+    );
+};
